12a5df57a8f477ba693b97cdace349f5c1f1d730
Renan Tarouco da Fonseca <renantarouco@gmail.com>
2020-12-22 21:27:16 -0300

    Inicial

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/totdev/go-utils
+
+go 1.15
+
+require github.com/gin-gonic/gin v1.6.3
---
+++ ping.go
@@ -0,0 +1,16 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// PingHandler returns a handler function for API's PING
+func PingHandler(apiName string) func(*gin.Context) {
+	return func(c *gin.Context) {
+		c.JSON(http.StatusOK, map[string]string{
+			"apiName": apiName,
+		})
+	}
+}
